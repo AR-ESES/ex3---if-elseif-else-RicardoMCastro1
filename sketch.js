d5965ccdcba61744318645d726cfc5d80eb13ed7
RicardoMCastro1 <210200112@ese.ipsantarem.pt>
2023-06-15 17:55:12 +0100

    codigo

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(800, 800);
+  }
+  
+  function draw() {
+    background(220);
+
+    fill(0,206,209)
+    circle(200, 200, 200)
+
+    fill(188,143,143)
+    circle(500,200,200)
+
+    fill(255,0,255)
+    circle(500,500,200)
+
+    fill(240,128,128)
+    circle(200,500,200)
+  }
